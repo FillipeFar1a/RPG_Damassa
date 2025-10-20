8b27e1dda063d0b921cb8463b88367d205dbffa6
fillipefaria1-ship-it <fillipefaria1@gmail.com>
2025-10-20 09:18:32 -0300

    Aura + ego
    super aura farmada

--- RPG_Damassa/src/personagens/herois/Aurora.java
+++ RPG_Damassa/src/personagens/herois/Aurora.java
@@ -4,7 +4,7 @@ import personagens.Personagem;
 
 public class Aurora extends Personagem {
     public Aurora() {
-        super("Aurora", "Maga", 28, 26, 8, 3, 1, 20);
+        super("Aurora", "Coelha Safada", 28, 26, 8, 3, 1, 20);
     }
     @Override public String[] intro() {
         return new String[]{
